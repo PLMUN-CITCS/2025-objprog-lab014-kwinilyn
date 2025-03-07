aa4dd0d741a8f0e2e0bb3617ea188b704252e794
kwinilyn <sabsalqueenielyn_bspsy@plmun.com>
2025-03-07 10:17:39 +0800

    Submitting OBJPROG Week 05 - Laboratory # 14

---
+++ MethodReturnTypes.java
@@ -0,0 +1,36 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        public class MethodReturnTypes {
+        public static void main(String args) {
+            displayWelcomeMessage();
+        }
+            //... (displayWelcomeMessage method)...
+        }
+        public class MethodReturnTypes {
+            //... (other methods)...
+        public static double calculateAverage(int num1, int num2) {
+            public class MethodReturnTypes {
+                //... (other methods)...
+            public static double calculateAverage(int num1, int num2) {
+            double average = (num1 + num2) / 2.0;
+            return average;
+            }
+            }
+        }
+        }
+        public class MethodReturnTypes {
+        public static void main(String args) {
+            //... (other code in main)...
+        System.out.println("The average is: " + result);
+        }
+             //... (other methods)...
+        }
+    }
+    public class MethodReturnTypes {
+        //... (main method)...
+    public static void displayWelcomeMessage() {
+     System.out.println("Welcome to our program!");
+    }
+    }
+
+}
